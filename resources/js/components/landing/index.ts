f28786f321013100c017edfa98a6c2b8635fd521
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2025-09-30 23:04:14 +0400

    Delete unused react components from home page

--- resources/js/components/landing/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Navigation } from './Navigation';
-export { default as HeroSection } from './HeroSection';
-export { default as FeaturesSection } from './FeaturesSection';
-export { default as TestimonialsSection } from './TestimonialsSection';
-export { default as PricingSection } from './PricingSection';
-export { default as FinalCTASection } from './FinalCTASection';
-export { default as FeaturedCoursesSection } from './FeaturedCoursesSection';
-export { default as Footer } from './Footer';
